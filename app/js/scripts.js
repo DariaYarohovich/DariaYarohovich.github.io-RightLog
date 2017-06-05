82112ac19ff580d3df754c75799d7819db4581ad
mozolevsky <alf.rulit@gmail.com>
2017-06-06 01:27:36 +0300

    Footer block

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -59,10 +59,38 @@ var swiper = new Swiper(".swiper-container", {
 	}
 });
 
-// option customize
-
+// select option customize
 (function($) {
     $(function() {
         $("select").styler();
     });
 })(jQuery);
+
+// yandex map
+
+ymaps.ready(init);
+
+function init () {
+    var myMap = new ymaps.Map("map", {
+            center: [53.848587, 27.661993],
+            zoom: 15,
+            behaviors: ['default', 'scrollZoom']
+        }),
+
+        myPlacemark2 = new ymaps.Placemark([53.849487, 27.664589], {
+        }, {
+            // Опции.
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/marker.png',
+            // Размеры метки.
+            iconImageSize: [23, 37],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [0, 0]
+        });
+
+    // Добавляем все метки на карту.
+    myMap.geoObjects.add(myPlacemark2);
+    myMap.controls.add('zoomControl', {right: '5px', top: '50%'});
+
+}
